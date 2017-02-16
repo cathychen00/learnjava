aa2312aebec0227d77a7351d5f4464645c77e716
chenjing <chenjing@autohome.com.cn>
2017-02-16 17:04:45 +0800

    mybatis demo init

--- DemoStoreData/src/main/resources/db/store-schema.sql
+++ DemoStoreData/src/main/resources/db/store-schema.sql
@@ -1,20 +1,17 @@
 create table category (
-	catid varchar(10) not null,
-	name varchar(80) null,
-	descn varchar(255) null,
-	constraint pk_category primary key (catid)
+catid varchar(10) not null,
+catename varchar(80) null,
+constraint pk_category primary key (catid)
 );
 
 create table product (
-    productid varchar(10) not null,
-    category varchar(10) not null,
-    name varchar(80) null,
-    descn varchar(255) null,
-    constraint pk_product primary key (productid),
-        constraint fk_product_1 foreign key (category)
-        references category (catid)
+productid varchar(10) not null,
+cateid varchar(10) not null,
+productName varchar(80) null,
+constraint pk_product primary key (productid),
+constraint fk_product_1 foreign key (cateid)
+references category (catid)
 );
 
-create index productCat on product (category);
-create index productName on product (name);
-
+create index productCat on product (cateid);
+create index productName on product (productName);

0d14ba8179a829a9bb7cf93dba550eb9a8d04133
chenjing <chenjing@autohome.com.cn>
2017-03-16 13:51:50 +0800

    bean xml配置 c:

--- DemoStoreDI/src/main/java/Main.java
+++ DemoStoreDI/src/main/java/Main.java
@@ -11,8 +11,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args){
 
-//        testXmlConfig();
-        testJavaConfig();
+        testXmlConfig();
+//        testJavaConfig();
 
     }
 

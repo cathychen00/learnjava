18ecca16120f7ba5fc45e175e431d1b5c91120e1
chenjing <chenjing@autohome.com.cn>
2017-03-20 10:30:47 +0800

    aop javaconfig调通

--- DemoAOP/src/main/java/javaConfig/BeanConfig.java
+++ DemoAOP/src/main/java/javaConfig/BeanConfig.java
@@ -4,18 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import service.CategoryService3;
 
 /**
  * Created by 陈敬 on 2017/3/17.
  */
 @Configuration
 @EnableAspectJAutoProxy
-@ComponentScan
+@ComponentScan(basePackageClasses = {service.CategoryService3.class,pointcut.LogHelper3.class})
 public class BeanConfig {
 
-    @Bean
-    public CategoryService3 service(){
-        return new CategoryService3();
-    }
+//    @Bean
+//    public CategoryService3 service(){
+//        return new CategoryService3();
+//    }
 }
--- DemoAOP/src/main/java/pointcut/LogHelper3.java
+++ DemoAOP/src/main/java/pointcut/LogHelper3.java
@@ -2,14 +2,14 @@ package pointcut;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executor;
 
 /**
  * Created by 陈敬 on 2017/3/17.
  */
+@Component
 @Aspect
 public class LogHelper3 {
 

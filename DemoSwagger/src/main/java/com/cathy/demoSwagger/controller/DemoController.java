bc1f504292fb70369a9d2474d13383b47fe48ffa
chenjing <JYcj*0921b>
2017-11-07 13:50:29 +0800

    Springboot Rest Api

---
+++ DemoSwagger/src/main/java/com/cathy/demoSwagger/controller/DemoController.java
@@ -0,0 +1,24 @@
+package com.cathy.demoSwagger.controller;
+
+import com.cathy.demoSwagger.model.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @author 陈敬
+ * @date 17/11/7
+ */
+@RestController
+public class DemoController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
